f24b007b23457f016468c440d5e3d4c77b8d0029
Anazri <axellisoragatha4@gmail.com>
2026-03-26 15:02:24 +0700

    add : sertifikasi

--- script.js
+++ script.js
@@ -182,3 +182,17 @@ function toggleAccordion(btn) {
     var item = btn.parentElement;
     item.classList.toggle('active');
 }
+
+function switchPortofolioTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("portofolio-tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active");
+    }
+    tablinks = document.getElementsByClassName("portofolio-tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
